6853c7358ea359111b6bade089f51ac7822b0bf9
Prince Sharma <princesharma0816@gmail.com>
2026-08-26 20:46:33 +0530

    Founndation

---
+++ src/test/java/com/einstein/EinsteinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.einstein;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EinsteinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
